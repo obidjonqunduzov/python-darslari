7c5490f8015394e0967dd38eb095a79a3377c067
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-09-09 17:01:57 +0300

    yangi_dars

---
+++ 17_dars.py
@@ -0,0 +1,80 @@
+
+# Funksiya qiymati bilam ishlash
+
+def toliq_ism_yasa(ism,familiya):
+	# toliq ism qaytaruvchi funksiya
+	toliq_ism = f"{ism} {familiya}"
+	return(toliq_ism) # return(qaytarish) degani
+
+toliq_ism_yasa("Obid",'Qunduzov')
+talaba = toliq_ism_yasa("Obid","Qunduzov")
+print(talaba)
+
+def toliq_ism_yasa(ism,familiya,otasini_ismi=''):
+
+	if otasini_ismi:
+		toliq_ism = f'{ism} {familiya} {otasini_ismi}'
+	else:
+		toliq_ism =f'{ism} {familiya}'
+	return toliq_ism.title()	
+
+talaba1 = toliq_ism_yasa("olim","hakimov")
+talaba2 = toliq_ism_yasa('obid','Qunduzov','komiljonovich')
+print(f"Darsga kelmagan talabalar: {talaba1} va {talaba2}")
+
+def avto_info(kompaniya, model, rangi, karobka, yili, narhi = None):
+	avto ={
+	'kompaniya':kompaniya,
+	'model':model,
+	'rangi':rangi,
+	'karobka':karobka,
+	'yili':yili,
+	'narh':narhi
+	}
+	return avto
+
+avto_1 = avto_info('opel','infinity','qora','avtomat',2020)
+avto_2 = avto_info('opel','astra','oq','mexanik',2018,12000)
+avtolar = [avto_1, avto_2]
+print("Onlayn bozordagi mavjud avtomashinalar: ")
+
+for avto in avtolar:
+	if avto['narh']:
+		narh = avto['narh']
+	else:
+		narh = 'Nomalum'
+	print(f"{avto['rangi']} {avto['model']}. Narhi: {narh}")
+
+def oraliq(min,max,qadam):
+	sonlar = []
+	while min<max:
+		sonlar.append(min)
+		min += 1
+	return sonlar
+print(oraliq(0,11,2))
+print(oraliq(11,21,2))
+
+print("Saytimizda avtomobillar ruyxatini shakllantiramiz!")
+
+avtolar = []
+while True:
+	print("\n Quyidagi malumotlarni kiriting", end = '')
+	kompaniya = input("\n Ishlab chiqaruvchi: ")
+	model = input("Modeli: ")
+	rangi = input("Rangi: ")
+	karobka = input("Karobka: ")
+	yili = input("Yili: ")
+	narhi = input("Narhi: ")
+	avtolar.append(avto_info(kompaniya,model,rangi,karobka,yili,narhi))
+
+	javob = input("Yana avto qoshasizmi? (ha/yoq): ")
+	if javob == 'yoq':
+		break
+print("Salondagi avtomoshinalar: ")
+
+for avto in avtolar:
+	if avto['narh']:
+		 narh = avto['narh']
+	else:
+		 narh = 'Nomalum'
+	print(f"{avto['rangi'].title()} {avto['model'].title()} {avto['karobka']} karobka, Narh: {narh}")
