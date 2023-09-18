d7420cd648dee90814341dc17bd4b2b608cf6738
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-09-18 23:30:21 +0300

    yangi_dars

--- dars_practikum.py
+++ dars_practikum.py
@@ -1,30 +1,21 @@
 # Soz topish oyini
 
-import random as ran
+import  random
 
-print("Keling soz topish oyinini oynaymiz! ")
+def sontop(x=10):
+	tasodifiy_son = random.randint(1,x)
+	print(f"Men 1 dan {x} gacha son oylayman, Siz topasiz")
+	taxminlar = 0
 
-n = 1
-
-
-while True:
-	son = ran.sample(range(10),1)
-	savol =f"1 dan 10 gacha son oyladim uni toping {n}: "
-	soz_kiritish = int(input(savol))
-	
-	n +=1
-
-
-	if soz_kiritish < son:
-		print("Harakat qiling, men oylagan son bundan kichik")
-	elif soz_kiritish  > son:
-		print("Men oylagan son bundan katta")
-	else:
-		
-		print("topdingiz! ")
-
-	#for topdi in savol:
-		#print(f"Siz {n} da topdingiz")
-	#print("Yana oynaymizmi?")
+	while True:
+		taxminlar +=1
+		taxmin = int(input(">>>"))
+		if taxmin<tasodifiy_son:
+			print("Xato! Men oylagan son bundan katta.Harakat qiling")
+		elif taxmin>tasodifiy_son:
+			print("Xato! Men oylagan son bundan kichik.Harakat qiling")
+		else:
+			break
+	print(f"Tabriklaymiz. {taxminlar} va txmin bilan topdingiz")	
 
 	
