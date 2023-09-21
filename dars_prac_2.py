c660ffe09fcf086b2eb01157ce4d8e922f3fac9f
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-09-21 16:46:38 +0300

    dars_davomi

--- dars_prac_2.py
+++ dars_prac_2.py
@@ -10,6 +10,7 @@ def get_word():
 		word = random.choice(words)
 	return word.upper()
 
+
 def display(user_letters,word):
 	display_letter = ""
 	for letter in word:
@@ -17,7 +18,7 @@ def display(user_letters,word):
 			display_letter +=letter
 		else:
 			display_letter += '-'
-		return display_letter
+	return display_letter
 
 def play():
 	word = get_word()
@@ -25,8 +26,20 @@ def play():
 	user_letters = ''
 	print(f"  Men {len(word)} xonali soz uyladim. Topa olasizmi?")
 
+	while len(word_letters)>0:
+		print(display(user_letters,word))
+		if len(user_letters)>0:
+			print(f"Shu vaqtgacha kiritgan harflaringiz! {user_letters}")
 
-	
-
-
+		letter = input("Harf kiriting: ").upper()
+		if letter in user_letters:
+			print("Bu harfni oldin kritgansiz.Boshqa harf kiriting: ")
+			continue
+		elif letter in word:
+			word_letters.remove(letter)
+			print((f"{letter} harfi tuqri"))
+		else:
+			print("Bunday harf yuq")
+		user_letters += letter
+	print(f"Tabriklayman! {word} suzini {len(user_letters)} ta urunishda topdingiz!.")
 
