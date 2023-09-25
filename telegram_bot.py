7fafc1918b1ff5788850b6cdee25e0bc4131df93
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-09-25 10:34:00 +0300

    dars-davomi

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,12 +1,26 @@
-
+import telebot
 from transliterate import to_cyrillic, to_latin
 
+TOKEN = "6651191354:AAGtmBrB6tBGBr04UVf4q7D9o3vve5FdKgA"  # <-- Tokeningizni shu yerga yozing
+bot = telebot.TeleBot(token=TOKEN)
 
-# isascii() jadval
+# \start komandasi uchun mas'ul funksiya
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    username = (
+        message.from_user.username
+    )  # Bu usul bilan foydalanuvchi nomini olishimiz mumkin
+    xabar = f"Assalom alaykum, {username} Kirill-Lotin-Kirill botiga xush kelibsiz!"
+    xabar += "\nMatningizni yuboring."
+    bot.reply_to(message, xabar)
 
-matn = input(" 'lotin' yoki 'криллда' Soz kiriting: ")
 
-if matn.isascii():
-	print(to_cyrillic(matn))
-else:
-	print(to_latin(matn))
+# matnlar uchun mas'ul funksiya
+@bot.message_handler(func=lambda msg: msg.text is not None)
+def translit(message):
+    msg = message.text
+    javob = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+    bot.reply_to(message, javob(msg))
+
+
+bot.polling()
