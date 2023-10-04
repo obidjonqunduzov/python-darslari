76fe87bc7ecef64157234b17f5cb4ce45904bd5d
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-10-04 16:44:52 +0300

    dars_davomi

--- dars_23_prac.py
+++ dars_23_prac.py
@@ -36,5 +36,33 @@ avto_1.update_km()
 avto_1.update_km()
 avto_1.update_km()
 avto_1.update_km()
+avto_1.update_km()
 
 print(avto_1.get_info())
+
+class New_avto:
+	def __init__(self,salon):
+		self.salon = salon
+		self.yangi_avto = 0
+
+	def add_avto(self,saln_avto):
+		self.new_avto.append(saln_avto)	
+		self.yangi_avto += 1
+
+	def get_sal_avto(self):
+		"""Sotuvdagi avtomobillar"""
+		return [saln_avto.get_info() for saln_avto in self.new_avto]
+	def get_apd_avto(self):
+		return self.yangi_avto
+
+sl_avtolar = New_avto("Orion Avto Saloni")
+avto_1 = Avto('mersedes_benz','sls','qora','avtomat',25000,'germaniya')
+avto_2 = Avto('range rover','optima','qora','avtomat',45000,'angliya')
+avto_3 = Avto('tayota','prado','oq','mexanik',35000,'yaponiya')
+avto_4 = Avto('haval','x7','qizil','avtomat',14000,'xitoy')
+sl_avtolar.add_avto(avto_1)
+sl_avtolar.add_avto(avto_2)
+sl_avtolar.add_avto(avto_3)
+sl_avtolar.add_avto(avto_4)
+
+print(sl_avtolar.get_sal_avto())
