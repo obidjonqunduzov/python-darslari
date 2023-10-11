5c274e5894b6f29784075ef698470a04bc2e5ff7
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-10-11 12:29:23 +0300

    dars_davomi

--- dars_24.py
+++ dars_24.py
@@ -13,20 +13,44 @@ class Shaxs:
 info = Shaxs("Obidjon","Qunduzov",1998,"teacher")
 print(info.get_info())
 
+
+
 class Student(Shaxs): # Shaxs classining vorisi Talaba classi bo'ladi!
-	def __init__(self,name,lastname,age,professiy,facultet,uneversity):
+	def __init__(self,name,lastname,age,professiy,facultet,uneversity,live):
 		super().__init__(name,lastname,age,professiy)
 		self.facultet = facultet
 		self.uneversity = uneversity
+		self.live = live
 
 	def get_uneversity(self):
 		return self.uneversity
 
 	def get_facultet(self):
+
 		return self.facultet
 
+	def get_info(self):
+		return  f"{self.name.title()} {self.lastname.title()} , was born in Tajikistan he has  {2021 - self.age}, he a {self.professiy} in school, he read on {self.facultet} on uneversity of {self.uneversity}"
 
 
-student_1 = Student("obidjon","qunduzov",1997,"teacher2","fiztex","HGU")
 
-print(f"{student_1.get_info()}, he read on {student_1.get_facultet()} on uneversity of {student_1.get_uneversity()}")
+
+class Live:
+	"""Talabaning yashash joyini qaytaradi"""
+	def __init__(self, street,city,region):
+		self.street = street
+		self.city = city
+		self.region = region
+
+	def get_info_live(self):
+		return f"{student_1.get_info()},he live in  {self.region.title()}, {self.city.title()}, {self.street.title()}"
+
+
+
+
+
+
+student_live = Live("f.otakhonov 47","khujand","sugd")
+student_1 = Student("obidjon","qunduzov",1997,"teacher2","fiztex","HGU",student_live)
+
+print(student_1.live.get_info_live())
