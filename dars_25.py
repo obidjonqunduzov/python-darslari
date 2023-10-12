dd049450e5ead82337fa82984b79e25fe067da13
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-10-12 15:30:37 +0300

    dars_davomi

--- dars_25.py
+++ dars_25.py
@@ -1,16 +1,23 @@
 
 # OOD . Inkapsulatsiya bilan tanishamiz!
+# __bu degani inkapulatsiya qilingan degani
 
 from uuid import uuid4
 
 class Book:
 	""" Book classi """
+	__num_book = 0
 	def __init__(self,name_bk,payge,price,discount=0):
 		self.name_bk = name_bk
 		self.payge = payge
 		self.price = price
-		self.__discount = discount
+		self.__discount = discount # bu yashirin, tahqaridan murojat qiqib bo'lmaydi!
 		self.__id = uuid4()
+		Book.__num_book += 1
+
+	@classmethod
+	def get_num_book(cls):
+		return cls.__num_book
 
 	def get_discount(self):
 		return self.__discount
@@ -25,13 +32,13 @@ class Book:
 		else:
 			print("Cegirmani kamatirib bo'lmaydi!")
 
+
 	def get_book_info(self):
 
-		book = f"{self.name_bk.title()} nomli kitob, {self.payge} sahifa, {self.price} $"
+		book = f"{self.name_bk.title()} nomli kitob, {self.payge} sahifa, {self.price} $"     
 		return book
-	def get_id_book(self):
-		return f"{read_book.get_book_info()} kitobini dokonimizdan {read_book.get_discount()} % chegirma orqali xarid qilishingiz mumkin! ID {read_book.get_id()}"
 
-read_book = Book("Amallar niyatga bog'liq", 180, 7,15)
-read_book.add_discount(15)
-print(read_book.get_id_book())
+	def get_id_book(self):
+		return f"{self.get_book_info()} kitobini dokonimizdan\n {self.get_discount()} % chegirma orqali xarid qilishingiz mumkin! ID {self.get_id()}"
+
+# davomi dars_25_next.py faylida saqlanyapdi
---
+++ dars_25_next.py
@@ -0,0 +1,15 @@
+from dars_25 import Book
+
+read_book_1 = Book("Amallar niyatga bog'liq", 180, 7,15)
+
+
+read_book_2 = Book("uyla va boy bo'l",245,10,10)
+
+read_book_3 = Book("Hadis va Hayot",561,15,20)
+
+read_book_4 = Book("olam va odam",154,7,12)
+
+read_book_5 = Book("Good to Great",402,25,30)
+
+print(read_book_5.get_id_book())
+print(read_book_4.get_num_book())
