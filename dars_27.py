b60f0d75c07c3afae24415c1a1b144cb69cc91d2
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-10-18 17:11:35 +0300

    dars_davomi

--- dars_27.py
+++ dars_27.py
@@ -1,9 +1,9 @@
 # Fayllar bilan ishlashni o'rganamiz!
 
-file = open("pi.txt.py")
-PI = file.read()
-print(PI)
-file.close()
-
-with open('pi.txt') as fayl:
+with open("pi.txt") as fayl:
     pi = fayl.read()
+
+pi = pi.rstrip() # qator ohiridagi bo'shliqlarni olib tashlaymiz
+pi = pi.replace('\n','') # qator tashlash belgisini almashtiramiz
+pi = float(pi) # matnni float (o'nlik) songa o'tkazamiz
+print(pi)
