d31ae044cf931b4a5e4d5b5b98fbed3afa5791ee
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-11-06 17:15:55 +0300

    dars_davomi

--- dars_30.py
+++ dars_30.py
@@ -62,3 +62,54 @@ with open(filename) as f:
 print(f"{bemor} \n") # bu oddiy print
 
 pprint(bemor) # bu chiroyli print
+
+
+# RegEx andoza yordamida matn izlash
+
+import re
+
+word1 = 'темир'
+word2 = 'томир'
+word3 = 'тулпор'
+
+andoza ='^т...р$'
+
+print(re.match(andoza,word1))
+print(re.match(andoza,word2))
+print(re.match(andoza,word3))
+
+from uzwords import words
+
+andoza ='^б....а$'
+matches = []
+
+for word in words:
+	if re.match(andoza,word):
+		matches.append(word)
+print(matches)
+
+
+matn = """Вы всегда можете задать нам любой вопрос в разделе Обращения 
+в Личном кабинете или воспользоваться одним из мессенджеров
+Чтобы рассылка не попадала в спам, добавьте info@wildberries.ru в адресную книгу"""
+
+andoza = '[^@ \t\r\n]+@[^@ \t\r\n]+\\.[^@ \t\r\n]+'
+
+email = re.findall(andoza,matn)
+print(email)
+
+
+# Kuchli parolni tekshirish
+# Quyidagi andoza ham ihateregex.io sahifasidan olindi
+andoza = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$'
+msg = "Yangi parol kiriting"
+msg += '(kamida 8 belgidan iborat, kamida 1 ta lotin bosh harf, 1 ta kichik harf, '
+msg += '1 ta son va 1 ta maxsus belgi boʻlishi kerak): '
+
+while True:
+    password = input(msg)
+    if re.match(andoza,password):
+        print("Maxfiy so'z qabul qilindi")
+        break
+    else:
+        print("Maxfiy so'z talabga javob bermadi")
