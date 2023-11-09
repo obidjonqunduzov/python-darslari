64d259e011b37aae824133768e63fd054890f71f
obidjonqunduzov <obidjonqunduzov1998@gmail.com>
2023-11-09 16:45:21 +0300

    yangi_dars

---
+++ dars_31.py
@@ -0,0 +1,8 @@
+
+# PIP VA TASHQI KUTUBXONALAR
+
+from googletrans import Translator
+tarjimon = Translator() # Translator bu maxsus klass (tarjimon esa obyekt)
+matn_uz = "Python - dunyodagi eng kuchli dasturlash tili"
+tarjima = tarjimon.translate(matn_uz)
+print(tarjima.text)
